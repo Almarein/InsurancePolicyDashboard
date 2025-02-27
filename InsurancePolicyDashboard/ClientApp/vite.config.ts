25bb26d75bb3990f0e66e886d594e014299d8d7b
Roman Alekseev <romana@frankcrum.com>
2025-02-27 14:54:54 +0200

    Https certificate creation was returned to vite config

--- InsurancePolicyDashboard/ClientApp/vite.config.ts
+++ InsurancePolicyDashboard/ClientApp/vite.config.ts
@@ -4,13 +4,35 @@ import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 import fs from 'fs';
 import path from 'path';
+import child_process from 'child_process';
 import { env } from 'process';
 
-const baseFolder = `${env.HOME}/.aspnet/https`;
+const baseFolder =
+    env.APPDATA !== undefined && env.APPDATA !== ''
+        ? `${env.APPDATA}/ASP.NET/https`
+        : `${env.HOME}/.aspnet/https`;
 
 const certificateName = "insurancepolicydashboard";
 const certFilePath = env.SSL_CRT_FILE || path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = env.SSL_KEY_FILE || path.join(baseFolder, `${certificateName}.key`);
+
+if (!fs.existsSync(baseFolder)) {
+    fs.mkdirSync(baseFolder, { recursive: true });
+}
+
+if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+    if (0 !== child_process.spawnSync('dotnet', [
+        'dev-certs',
+        'https',
+        '--export-path',
+        certFilePath,
+        '--format',
+        'Pem',
+        '--no-password',
+    ], { stdio: 'inherit', }).status) {
+        throw new Error("Could not create certificate.");
+    }
+}
 
 const target = 'https://localhost:3001';
 
